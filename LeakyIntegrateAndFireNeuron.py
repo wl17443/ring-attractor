b9884ec7113385eabca61bb956915162ab595bd4
Angela <wl17443@bristol.ac.uk>
2020-07-22 00:39:49 +0100

    multiple synaptically connected neurons. Need to work on 1:1:1...:1n neurons

--- LeakyIntegrateAndFireNeuron.py
+++ LeakyIntegrateAndFireNeuron.py
@@ -24,31 +24,33 @@ class LeakyIntegrateAndFireNeuron:
     Rmgs = 0.15
     maxs = 0.5
     Ts = 10*ms 
-    # Es = -80*mV 
 
     T = 1
     dt = 0.25*ms 
     t = np.linspace(0,T,int(T/dt)+1)
 
-    def __init__(self, Id, neurontype, siblings, sibling_conns, main_conn):
+    def __init__(self, Id, neurontype, to_siblings_conns, from_siblings_conns, main_conn):
         # TODO Should contain all the neuron's it's connected to
         # Includes getting info from and getting info to
         self.id = Id 
         # TODO multiple synaptic weights 
-        self.Psynapses = np.zeros(len(siblings))
+        self.Psynapses = np.zeros(len(from_siblings_conns))
         self.spikeTrain = np.zeros(len(self.t))
-        self.siblings = siblings
-        self.sibling_conns = sibling_conns 
+        # self.siblings = siblings
+        self.to_siblings_conns = to_siblings_conns 
+        self.from_siblings_conns = from_siblings_conns
+        self.to_siblings = len(to_siblings_conns)
+        self.from_siblings = len(from_siblings_conns)
         self.main_conn = main_conn
         # TODO multiple presynaptic voltages 
         # Get 1 for fired and 0 for not fired from coupled neuron(s)
-        self.fire_signals = np.zeros(len(siblings))
+        self.fire_signals = np.zeros(len(from_siblings_conns))
         self.Es = neuronType[neurontype]
         # TODO add output neuron (boolean) parameter 
         # TODO add layer number as parameter (?)
 
-    def f(self, v, ps):
-        return (self.El-v-self.Rmgs*ps*(v-self.Es)+self.RmIe)/self.Tm
+    def f(self, v):
+        return (self.El-v-(self.Rmgs*self.Psynapses*(v-self.Es)).sum()+self.RmIe)/self.Tm
     
     def p(self, ps):
         return -ps/self.Ts
@@ -59,28 +61,39 @@ class LeakyIntegrateAndFireNeuron:
         # Get spike data from sibling neurons, if any 
         for i in range(1,len(self.t)):
             # Send and receive spiked (boolean) from coupled neuron(s)
-            for sibling in range(len(self.siblings)):
-                if self.id%2 == 0:
-                    self.fire_signals[sibling] = self.sibling_conns.recv()
-                    self.sibling_conns.send(self.spikeTrain[i-1]>=self.thresholdPotential)
-                else:
-                    self.sibling_conns.send(self.spikeTrain[i-1]>=self.thresholdPotential)
-                    self.fire_signals[sibling] = self.sibling_conns.recv()
-
+            # Even neurons send first then receive 
+            if self.id%2==0:
+                if self.to_siblings > 0:
+                    for to_sibling in self.to_siblings_conns:
+                        to_sibling.send(self.spikeTrain[i-1]>=self.thresholdPotential)
+                # for from_sibling in self.from_siblings_conns:
+                #     self.fire_signals[from_sibling] = from_sibling.recv()
+                if self.from_siblings > 0:
+                    for from_sibling_nr in range(self.from_siblings):
+                        self.fire_signals[from_sibling_nr] = self.from_siblings_conns[from_sibling_nr].recv()
+            else:
+                if self.from_siblings > 0:
+                    for from_sibling_nr in range(self.from_siblings):
+                        self.fire_signals[from_sibling_nr] = self.from_siblings_conns[from_sibling_nr].recv()
+                if self.to_siblings > 0:
+                    for to_sibling in self.to_siblings_conns:
+                        to_sibling.send(self.spikeTrain[i-1]>=self.thresholdPotential)
+        
             # With voltage from coupled neuron, if it's spiked increase synaptic weight 
             #   else decay with time constant
-            for sibling in range(len(self.siblings)):
-                if self.fire_signals[sibling]:
-                    self.Psynapses[sibling] += self.maxs
-                else:
-                    self.Psynapses[sibling] = self.Psynapses[sibling] + self.p(self.Psynapses[sibling])*self.dt
+            if self.from_siblings > 0:
+                for sibling in range(self.from_siblings):
+                    if self.fire_signals[sibling]:
+                        self.Psynapses[sibling] += self.maxs
+                    else:
+                        self.Psynapses[sibling] = self.Psynapses[sibling] + self.p(self.Psynapses[sibling])*self.dt
                 
             # Using Euler's method, calculate the current membrane potential 
             if self.spikeTrain[i-1] >= self.thresholdPotential:
                 self.spikeTrain[i-1] = 0*mV
                 self.spikeTrain[i] = self.resetPotential 
             else: 
-                self.spikeTrain[i] = self.spikeTrain[i-1]+self.f(self.spikeTrain[i-1], self.Psynapses)*self.dt
+                self.spikeTrain[i] = self.spikeTrain[i-1]+self.f(self.spikeTrain[i-1])*self.dt
 
         # Upon simulation complete, send spike train to mother process 
         self.main_conn.send(self.spikeTrain)
--- Main.py
+++ Main.py
@@ -3,18 +3,18 @@ import multiprocessing as mp
 from ScalingTools import *
 import numpy as np 
 import matplotlib.pyplot as plt
-manager = mp.Manager()
+import random as rnd 
 
 # Simulation times 
 SIMULATION_TIME = 1
 TIME_STEP = 0.25*ms
 TOTAL_TIME_STEPS = np.linspace(0, SIMULATION_TIME, int(SIMULATION_TIME/TIME_STEP)+1)
 
-NR_OF_LIAF_NEURONS = 2
+NETWORK_STRUCTURE = [5, 1]
+NR_OF_LIAF_NEURONS = np.sum(NETWORK_STRUCTURE)
 LIAF_NEURONS = []
 NEURON_SPIKE_TRAINS = []
 PROCESSES = []
-LAYERS = 2 
 
 T = 1
 dt = 0.25*ms 
@@ -30,24 +30,34 @@ if __name__ == '__main__':
         # Create mother to children processes pipes 
         pa,c = mp.Pipe()
         MOTHER_PROCESS_CONN_ENDS.append(pa)
-        # TODO randomly choose which neurons are excitatory/inhibitory according to a probability (?)
-        LIAF_NEURONS.append(LeakyIntegrateAndFireNeuron(Id=i, neurontype='i', siblings=[np.abs(i-1)], sibling_conns=None, main_conn=c))
+        # Randomly choose which neurons are excitatory/inhibitory according to a probability (?)
+        if rnd.random() >= 0.5:
+            ntype = 'i'
+        else: ntype = 'e'
+        LIAF_NEURONS.append(LeakyIntegrateAndFireNeuron(Id=i, neurontype=ntype, to_siblings_conns=[], from_siblings_conns=[], main_conn=c))
     
     # TODO connect to a random number of next layer neurons determined by user 
-    
+    # This ultimately defines how the neurons are connected to each other 
+    if len(NETWORK_STRUCTURE) > 1:
+        for layer_nr in range(len(NETWORK_STRUCTURE)-1):
+            for neuron_nr in range(NETWORK_STRUCTURE[layer_nr]):
+                end1, end2 = mp.Pipe()
+                LIAF_NEURONS[neuron_nr].to_siblings_conns.append(end1)
+                LIAF_NEURONS[(layer_nr+1)*NETWORK_STRUCTURE[layer_nr]].from_siblings_conns.append(end2)
+                
+
     # Manually adding conns for two neurons 
-    end1, end2 = mp.Pipe()
-    LIAF_NEURONS[0].sibling_conns = end1
-    LIAF_NEURONS[1].sibling_conns = end2
-    
+    # end1, end2 = mp.Pipe()
+    # LIAF_NEURONS[0].sibling_conns = end1
+    # LIAF_NEURONS[1].sibling_conns = end2
+
     # TODO Create a layer of neurons to a single neuron 
 
     # Start parallelised neuron processes
-    p1 = mp.Process(target=LIAF_NEURONS[0].simulate)
-    p2 = mp.Process(target=LIAF_NEURONS[1].simulate)
-
-    p1.start()
-    p2.start()
+    for neuron in LIAF_NEURONS:
+        PROCESSES.append(mp.Process(target=neuron.simulate))
+    for process in PROCESSES:
+        process.start()
 
     for pa in MOTHER_PROCESS_CONN_ENDS:
         spikeTrain = pa.recv()
