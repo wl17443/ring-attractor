1111a134cf2eb06ca53b698a38c2aff7d46cc765
Thinkpad <ginkobab@gmail.com>
2020-07-22 19:59:56 +0200

    added poisson input, cleaned code

--- neuron_w_synapses/lif_model.py
+++ neuron_w_synapses/lif_model.py
@@ -1,59 +1,46 @@
 # Leaky integrate and fire model
-import numpy as np
-import matplotlib.pyplot as plt
 
 ms = 0.001
 mV = 0.001
-bigMOhm = 1000000
-nA = 0.000000001
 
 
 class lif:
-    membraneTimeConstant = 20.0 * ms
-    leakyReversalPotential = -70.0 * mV
-    exSynapticReversalPotential = 0.0 * mV
-    inSynapticReversalPotential = -80.0 * mV
-    resetPotential = -80.0 * mV
-    membraneResistance = 10.0 * bigMOhm
-    electrodeInputCurrent = 0.0 * nA
-    thresholdPotential = -54.0 * mV
-    synapticTimeConstant = 10.0 * ms
-    Pmax = 0.5
+    tau = 20.0 * ms
+    Rl = -70.0 * mV
+    Pr = -80.0 * mV
+
+    Ip = 0.0  # Input from poisson
+    Pthr = -54.0 * mV
 
     def __init__(self, ID):
         self.id = ID
-        self.synapticCurrent = 0.0
-        self.synapses = {} # neuron: weight
+        self.Is = 0.0
+        self.synapses = {}  # neuron: weight
 
-        self.potential = self.resetPotential
-
+        self.V = self.Pr
 
     def step(self):
         self._hyperpolarize()
 
-        self._updateSynapticCurrent()
-        self._updatePotential()
+        self._updateIs()
+        self._updateV()
         self._depolarize()
 
-    def _updateSynapticCurrent(self):
-        self.synapticCurrent = 0.0
-
-        for neuron, weight in self.synapses.items():
-            if neuron.potential == 0.0:
-                self.synapticCurrent += weight
-            
-
-    def _updatePotential(self):
-        self.potential += (
-            self.leakyReversalPotential - self.potential
-            + self.synapticCurrent * self.membraneTimeConstant
-            + self.membraneResistance * self.electrodeInputCurrent) / self.membraneTimeConstant * ms
+    def _updateV(self):
+        self.V += (self.Rl - self.V + self.Is * self.tau) / self.tau * ms
 
     def _hyperpolarize(self):
-        if self.potential == 0.0:
-            self.potential = self.resetPotential
+        if self.V == 0.0:
+            self.V = self.Pr
 
     def _depolarize(self):
-        if self.potential >= self.thresholdPotential:
-            self.potential = 0.0
+        if self.V >= self.Pthr:
+            self.V = 0.0
 
+    def _updateIs(self):
+        self.Is = 0.0
+        self.Is += self.Ip
+
+        for neuron, weight in self.synapses.items():
+            if neuron.V == 0.0:
+                self.Is += weight
