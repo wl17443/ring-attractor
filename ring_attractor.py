4ca6657dcbb40bf07f80b33ed9fe1570fa2e44a2
Thinkpad <ginkobab@gmail.com>
2020-08-29 15:23:34 +0200

    formatted

--- ring_attractor.py
+++ ring_attractor.py
@@ -14,8 +14,8 @@ class RingAttractor:
                  n=128,
                  noise=2.0e-3,
                  weights=(0.050, 0.100, 0.050, 0.250),
-                 fixed_points_number=0, 
-                 time=300,
+                 fixed_points_number=0,
+                 time=1000,
                  plot=False):
 
         self.n = n
@@ -24,6 +24,7 @@ class RingAttractor:
         self.fp_n = fixed_points_number
         self.time = time
         self.plot = plot
+        self.mid_point = None
 
         self.neurons = [LIF(ID=i, angle=360.0/n*i, noise_mean=0, noise_std=self.noise,)
                         for i in range(n)]
@@ -34,25 +35,25 @@ class RingAttractor:
 
     def simulate(self):
 
-        mid_point = self.get_mid_point()
+        self.get_mid_point()
 
         potentials = [[] for _ in range(self.n)]
         for t in range(self.time):
             for neuron in self.neurons:
 
-                self.input_source(mid_point=mid_point, n_of_spikes=5,
-                                  weight=self.weights[0], begin_time=0, neuron=neuron, time=t)
+                self.input_source(n_of_spikes=5, begin=0,
+                                  neuron=neuron, time=t)
                 neuron.step()
                 potentials[neuron.id].append(neuron.V)
 
-        df, e = self.compute_gain(potentials, mid_point)
+        df, err = self.compute_loss(potentials)
 
         if self.plot:
-            self.plot_potentials(df, e)
+            self.plot_potentials(df, err)
 
-        return e
+        return err
 
-    def compute_gain(self, potentials, mid_point):
+    def compute_loss(self, potentials):
         df = pd.DataFrame(potentials)
         df.index = [self.neurons[i].angle for i in df.index]
         spikes = df == 0.0
@@ -64,20 +65,18 @@ class RingAttractor:
 
         means = spikes.apply(circular_mean, axis=0)
         total_mean = circular_mean(means)
-        error = np.abs(self.neurons[mid_point].angle - total_mean)
+        err = np.abs(self.neurons[self.mid_point].angle - total_mean)
 
-        print("total_mean", total_mean)
-        print(self.neurons[mid_point].angle)
-        return df, error
+        return df, err
 
-    def input_source(self, mid_point, n_of_spikes, weight, begin_time, neuron, time):
-        sources = [i for i in range(mid_point - 2, mid_point + 3)]
+    def input_source(self, n_of_spikes, begin, neuron, time):
+        sources = [i for i in range(self.mid_point - 2, self.mid_point + 3)]
 
-        if time > begin_time:
+        if time > begin:
             if neuron.id in sources:
                 for _ in range(n_of_spikes):
                     neuron.exc_ps_td.append(
-                        ((time - begin_time) * 1e-3, weight))
+                        ((time - begin) * 1e-3, self.weights[0]))
 
     def connect_with_fixed_points(self):
         for neur in self.neurons:
@@ -119,32 +118,28 @@ class RingAttractor:
         if self.fp_n <= 1:
             return self.n // 2
 
-
         free_points = set(np.arange(self.n)) - set(self.fixed_points)
         median = [*free_points][len(free_points) // 4]
 
-
         high = self.fixed_points[self.fixed_points > median][0]
         low = self.fixed_points[self.fixed_points < median][-1]
-        mid_point = (high + low) // 2
+        self.mid_point = (high + low) // 2
 
-        return mid_point
-
-
-
-    def plot_potentials(self, df, error):
+    def plot_potentials(self, df, err):
         _, ax = plt.subplots(figsize=(10, 10))
-        sns.heatmap(df, vmin=-0.08, vmax=0.0, cmap="viridis", xticklabels=int(self.time/10), yticklabels=10, cbar_kws={'label': "Membrane Potential (V)"}, ax=ax)
+        sns.heatmap(df, vmin=-0.08, vmax=0.0, cmap="viridis", xticklabels=int(self.time/10),
+                    yticklabels=10, cbar_kws={'label': "Membrane Potential (V)"}, ax=ax)
         plt.xlabel("Time (ms)")
         plt.ylabel("Orientation of neuron")
         plt.subplots_adjust(left=0.07, bottom=0.07, right=0.97, top=0.89)
 
-        ax.set_yticklabels(labels)
+        ax.set_title("Number of fixed points: {}\nNoise: {:.3e}\nWeights: {}\nError: {:.3f}".format(
+            self.fp_n, self.noise, self.weights, err))
 
-        ax.set_title("Number of fixed points: {}\nNoise: {:.3e}\nWeights: {}\nError: {:.3f}".format(self.fp_n, self.noise, self.weights, error))
-
-        plt.savefig( f"images/{datetime.now().strftime('%d-%m-%Y, %H:%M:%S')}.png")
+        plt.savefig(
+            f"images/{datetime.now().strftime('%d-%m-%Y, %H:%M:%S')}.png")
         plt.show()
+
 
 if __name__ == "__main__":
 
--- simulation_loop.py
+++ simulation_loop.py
@@ -1,9 +1,8 @@
-import concurrent.futures 
+import concurrent.futures
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from ring_attractor import RingAttractor
-from utils import calculate_weights
 
 weights = [0.050, 0.088, 0.050, 0.25]  # ext, inh, fp ext, inh
 fixed_points = [0, 1, 2, 4, 8, 16, 32]
@@ -20,25 +19,24 @@ noises_idx = ["{:.2E}".format(i) for i in noises]
 records = pd.DataFrame(index=fixed_points_idx, columns=noises_idx)
 
 
-def iteration(_noise, _fp_n): 
+def iteration(_noise, _fp_n):
     ring = RingAttractor(n=neurons_n, noise=_noise, weights=weights, fixed_points_number=_fp_n, time=time)
     e = ring.simulate()
 
     return e
+
 
 for n_idx, noise in tqdm(zip(noises_idx, noises)):
     for fp_idx, fp_n in zip(fixed_points_idx, fixed_points):
 
         errors = []
         with concurrent.futures.ProcessPoolExecutor() as executor:
-            results = [executor.submit(iteration, noise, fp_n) for _ in range(iterations)]
+            results = [executor.submit(iteration, noise, fp_n)
+                       for _ in range(iterations)]
 
             for f in concurrent.futures.as_completed(results):
                 errors.append(f.result())
 
-
         records.loc[fp_idx, n_idx] = np.mean(errors)
 
     records.to_csv("error_by_noise.csv")
-
-
