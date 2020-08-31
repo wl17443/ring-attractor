3f06214e56d50693e4fa29ca31c8473b7b7a5cb7
Thinkpad <ginkobab@gmail.com>
2020-08-31 02:03:28 +0200

    better parallelization

--- simulation_loop.py
+++ simulation_loop.py
@@ -1,38 +1,52 @@
 import concurrent.futures
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 from ring_attractor import RingAttractor
+
+neurons_n = 256
+time = 100
+iterations = 1
+
+noise_levels = 15
+noise_low = 0.0
+noise_high = 1.5e-3
 
 weights = [0.050, 0.100, 0.050, 0.250]  # ext, inh, fp ext, inh
 fixed_points = [0, 1, 2, 4, 8, 16, 32]
-time = 10000
-iterations = 1
-noise_levels = 15
-neurons_n = 256
 
-noises = np.linspace(0.0e-3, 1.5e-3, noise_levels)
-fixed_points_idx = [str(i) for i in fixed_points]
+noises = np.linspace(noise_low, noise_high, noise_levels)
 noises_idx = ["{:.2E}".format(i) for i in noises]
+fixed_points_idx = [str(i) for i in fixed_points]
 
-records = pd.DataFrame(index=fixed_points_idx, columns=noises_idx)
+records = [pd.DataFrame(index=fixed_points_idx, columns=noises_idx) for _ in noises]
+seeds = records.copy()
+results = []
 
 
-def iteration(_noise, _fp_n):
-    ring = RingAttractor(n=neurons_n, noise=_noise, weights=weights, fixed_points_number=_fp_n, time=time)
+def iteration(_noise, _fp_n, seed, ID):
+    ring = RingAttractor(n=neurons_n, noise=_noise, weights=weights, fixed_points_number=_fp_n, time=time, random_seed=seed)
     e = ring.simulate()
 
-    return e
+    return e, noises_idx[ID], str(_fp_n), seed, ID
 
 
-for n_idx, noise in tqdm(zip(noises_idx, noises)):
-    for fp_idx, fp_n in zip(fixed_points_idx, fixed_points):
+if __name__ == "__main__":
+    with concurrent.futures.ProcessPoolExecutor() as executor:
 
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            results = [executor.submit(iteration, noise, fp_n) for _ in range(iterations)]
 
-            errors = [f.result() for f in concurrent.futures.as_completed(results)]
+        for i, noise in enumerate(noises):
+            for fp_n in fixed_points:
+                for _ in range(iterations):
+                    results.append(executor.submit(iteration, noise, fp_n, np.random.choice(10000), i))
 
-        records.loc[fp_idx, n_idx] = np.mean(errors)
 
-    records.to_csv("error_by_noise.csv")
+    for f in concurrent.futures.as_completed(results):
+        error, noise_idx, fp_idx, seed, ID = f.result()
+        records[ID].loc[fp_idx, noise_idx] = error
+        seeds[ID].loc[fp_idx, noise_idx] = seed
+
+    for i, df in records:
+        df.to_csv("errors_it_{}.csv".format(i))
+
+    for i, df in seeds:
+        df.to_csv("seeds_it_{}.csv".format(i))
