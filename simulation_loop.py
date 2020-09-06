5d942944d81fb82b70f55e4b6b36edf0cf1a408b
Thinkpad <ginkobab@gmail.com>
2020-09-06 11:49:23 +0200

    trying with 300 noise levels

--- simulation_loop.py
+++ simulation_loop.py
@@ -8,10 +8,10 @@ from ring_attractor import RingAttractor
 params = {
     "neurons_n": 256,
     "simulation_time": 10000,
-    "iterations": 30,
-    "noise_levels": 30,
+    "iterations": 1,
+    "noise_levels": 300,
     "noise_low": 0.0,
-    "noise_high": 3.0e-3,
+    "noise_high": 1.5e-3,
     "weights": [0.050, 0.100, 0.050, 0.250],  # ext, inh, fp ext, inh
     "fixed_points": [0, 1, 2, 4, 8, 16, 32]
 }
@@ -53,14 +53,13 @@ with ProcessPoolExecutor() as executor:
         error, noise_idx, fp_idx, it_n = f.result()
         records[it_n].loc[fp_idx, noise_idx] = error
 
-# for f in concurrent.futures.as_completed(results):
-#     error, noise_idx, fp_idx, it_n = f.result()
-#     records[it_n].loc[fp_idx, noise_idx] = error
 
-for i, df in enumerate(records):
-    df.to_csv("csv/singular_iters/seed_{}.csv".format(seeds[i]))
+# for i, df in enumerate(records):
+#     df.to_csv("csv/singular_iters/seed_{}.csv".format(seeds[i]))
 
-full_df = pd.concat(records).astype(float)
-df_average = full_df.groupby(full_df.index).mean()
+# full_df = pd.concat(records).astype(float)
+# df_average = full_df.groupby(full_df.index).mean()
 
-df_average.to_csv("csv/means.csv")
+# df_average.to_csv("csv/means.csv")
+
+records[1].to_csv("300_noise_levels.csv")
