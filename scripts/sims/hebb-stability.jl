28898ca682eb9d23ecaac380e9d5b5e28194bac2
Thinkpad <ginkobab@gmail.com>
2020-12-01 19:21:51 +0100

    prepared for remote hebb simulation

--- scripts/sims/hebb-stability.jl
+++ scripts/sims/hebb-stability.jl
@@ -6,16 +6,19 @@ using Statistics
 include(srcdir("hebb.jl"))
 include(srcdir("stability.jl"))
 
-h0 = 5e-7
+h0 = 5e-10
 h1 = 5e-6
 n0 = 0.0
 n1 = 2e-3
-step_h = 5e-8
-step_n = 2e-5
+step_h = 5e-9
+step_n = 2e-6
 seed=2020
 
 r_h = h0:step_h:h1
 r_n = n0:step_n:n1
+
+@show size(r_h)
+@show size(r_n)
 
 m = noise_vs_hebb(r_h, r_n, seed)
 tmp = @dict h0 h1 n0 n1 step_h step_n seed
